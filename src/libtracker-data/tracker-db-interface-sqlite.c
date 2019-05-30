c4816b8e8563426cdf04eb1c8897fa1d96134a03
Carlos Garnacho <carlosg@gnome.org>
2019-05-30 13:55:58 +0200

    libtracker-data: Add support for SERVICE{} syntax

--- src/libtracker-data/tracker-db-interface-sqlite.c
+++ src/libtracker-data/tracker-db-interface-sqlite.c
@@ -56,6 +56,7 @@
 #include "tracker-db-manager.h"
 #include "tracker-data-enum-types.h"
 #include "tracker-uuid.h"
+#include "tracker-vtab-service.h"
 #include "tracker-vtab-triples.h"
 #include "tracker-vtab-union.h"
 
@@ -3173,6 +3174,7 @@ tracker_db_interface_init_vtabs (TrackerDBInterface *db_interface,
 {
 	tracker_vtab_triples_init (db_interface->db, ontologies);
 	tracker_vtab_union_init (db_interface->db, ontologies);
+	tracker_vtab_service_init (db_interface->db, ontologies);
 	return TRUE;
 }
 
