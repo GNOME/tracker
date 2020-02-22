76405f3557950a41dddd1e9a455b0e6b18e4b379
Carlos Garnacho <carlosg@gnome.org>
2020-02-22 16:35:44 +0100

    tests: Make libtracker-data/sparql-test use in-memory databases
    This test has an ever growing amount of subtests, and each creates
    a SPARQL connection from scratch. Since we are testing SPARQL syntax
    and nothing about the underlying storage, use an in-memory database
    for those.

--- tests/libtracker-data/tracker-sparql-test.c
+++ tests/libtracker-data/tracker-sparql-test.c
@@ -376,8 +376,9 @@ test_sparql_query (TestInfo      *test_info,
 
 	data_location = g_file_new_for_path (test_info->data_location);
 
-	manager = tracker_data_manager_new (TRACKER_DB_MANAGER_FORCE_REINDEX,
-	                                    data_location, test_schemas, /* loc, domain and ontology_name */
+	manager = tracker_data_manager_new (TRACKER_DB_MANAGER_FORCE_REINDEX |
+	                                    TRACKER_DB_MANAGER_IN_MEMORY,
+	                                    NULL, test_schemas, /* loc, domain and ontology_name */
 	                                    FALSE, 100, 100);
 	g_initable_init (G_INITABLE (manager), NULL, &error);
 	g_assert_no_error (error);
