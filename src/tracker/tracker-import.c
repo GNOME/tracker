94866d69fa6508424c99321cc848239984be7861
Sam Thursfield <sam@afuera.me.uk>
2020-03-21 16:51:14 +0100

    Fixup tracker import and export

--- src/tracker/tracker-import.c
+++ src/tracker/tracker-import.c
@@ -67,7 +67,7 @@ create_connection (GError **error)
 
 		file = g_file_new_for_commandline_arg (database_path);
 		return tracker_sparql_connection_new (TRACKER_SPARQL_CONNECTION_FLAGS_NONE,
-		                                      file, NULL, error);
+		                                      file, NULL, NULL, error);
 	} else if (dbus_service && !database_path && !remote_service) {
 		GDBusConnection *dbus_conn;
 
